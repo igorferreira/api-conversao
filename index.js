97cd8d039d614c1f7ed55148e520c534281e2919
IGOR DOS SANTOS FERREIRA <igorferreira.noorden@sulamericaauto.com.br>
2020-11-17 00:15:35 -0300

    adicionado exibicao do ip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ip = require("ip");
 const os = require('os')
 const app = express();
 const swaggerUi = require('swagger-ui-express');
@@ -6,6 +7,14 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
+app.get('/', (req, res) => {
+    res.json({ "status": "OK", "maquina": os.hostname(), "ip": ip.address() });
+});
+
+app.get('/health', (req, res) => {
+    res.json({ "status": "OK", "maquina": os.hostname(), "ip": ip.address() });
+});
 
 app.get('/fahrenheit/:valor/celsius', (req, res) => {
 
